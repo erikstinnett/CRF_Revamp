47a3fc3bc0e995d082410cbbcb1cf2d24a6fe590
Erik Stinnett <estinnett@cub.uca.edu>
2025-10-29 11:35:08 -0500

    CRF website update

--- js/app.js
+++ js/app.js
@@ -1,8 +1,20 @@
 const hamMenu = document.querySelector(".ham-menu");
-
+const xMenu = document.querySelector(".nav-close");
 const offScreenMenu = document.querySelector(".off-screen-menu");
 
 hamMenu.addEventListener("click", () => {
-  hamMenu.classList.toggle("active");
   offScreenMenu.classList.toggle("active");
+});
+
+xMenu.addEventListener("click", () => {
+  offScreenMenu.classList.remove("active");
+});
+
+window.addEventListener("scroll", function () {
+  const navbar = document.querySelector(".navbar");
+  if (window.scrollY > 50) {
+    navbar.classList.add("scrolled");
+  } else {
+    navbar.classList.remove("scrolled");
+  }
 });
